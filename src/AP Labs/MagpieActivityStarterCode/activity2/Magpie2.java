66c2ef7a9a780a963f590a4c97b757e072e5ddff
0ffz <dan.voznyy@gmail.com>
2019-04-29 15:03:55 -0400

    AP labs

---
+++ src/AP Labs/MagpieActivityStarterCode/activity2/Magpie2.java
@@ -0,0 +1,85 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:
+ * <ul><li>
+ * Uses indexOf to find strings
+ * </li><li>
+ * Handles responding to simple words and phrases
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ *
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2 {
+    /**
+     * Get a default greeting
+     *
+     * @return a greeting
+     */
+    public String getGreeting() {
+        return "Hello, let's talk.";
+    }
+
+    /**
+     * Gives a response to a user statement
+     *
+     * @param statement the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement) {
+        String response = "";
+        if (statement.indexOf("no") >= 0) {
+            response = "Why so negative?";
+        } else if (statement.indexOf("mother") >= 0
+                || statement.indexOf("father") >= 0
+                || statement.indexOf("sister") >= 0
+                || statement.indexOf("brother") >= 0) {
+            response = "Tell me more about your family.";
+        } else if (statement.contains("dog")
+                || statement.contains("cat")) {
+            response = "Tell me more about your pets";
+        } else if (statement.contains("Ms. Krasteva"))
+            response = "She sounds like a great teacher";
+        else if(statement.trim().length() == 0)
+            response = "Say something, please.";
+        else if(statement.contains("money"))
+            response = "Do you happen to have your credit card information with you?";
+        else if(statement.contains("why?"))
+            response = "I'm stuck in Uganda, that's why.";
+        else if(statement.contains("Uganda"))
+            response = "Yeah, that's where I'm at right now!";
+        else {
+            response = getRandomResponse();
+        }
+        return response;
+    }
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     *
+     * @return a non-committal string
+     */
+    private String getRandomResponse() {
+        final int NUMBER_OF_RESPONSES = 6;
+        double r = Math.random();
+        int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+        String response = "";
+
+        if (whichResponse == 0) {
+            response = "Interesting, tell me more.";
+        } else if (whichResponse == 1) {
+            response = "Hmmm.";
+        } else if (whichResponse == 2) {
+            response = "Do you really think so?";
+        } else if (whichResponse == 3) {
+            response = "You don't say.";
+        } else if (whichResponse == 4) {
+            response = "It really do be like that.";
+        } else if (whichResponse == 5) {
+            response = "Hey, that's pretty good.";
+        }
+
+        return response;
+    }
+}
